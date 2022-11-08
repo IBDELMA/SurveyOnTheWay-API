96844ad1735779273f0b80b7615f9c5f6258abd4
Abdelmalek Ihdene <abdelmalek.ihdene@protonmail.com>
2022-11-07 21:42:38 -0500

    Add git script

--- listenToWebhooks.py
+++ listenToWebhooks.py
@@ -1,8 +1,12 @@
 from flask import Flask, request
+import git
+require("dotenv").config();
 app = Flask(__name__)
+g = git.cmd.Git(process.env.GIT_DIR)
 @app.route('/', methods=['POST'])
 def listen():
     print("Received Webhook!")
+    g.pull()
     return "", 200;
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
